a7833ff5b65cb495a0d1cb6e47ab94b5e90fa0cc
David Schwietering <d-schwietering@gmx.de>
2025-07-22 19:35:00 +0200

    WIP FS 25

--- LicensePlatesExtended.lua
+++ LicensePlatesExtended.lua
@@ -1,7 +1,7 @@
 LicensePlateStorage = {
 	data = {},
 	dataByVehicle = {},
-	debugActive = false,
+	debugActive = true,
 	MOD_NAME = g_currentModName,
 	BASE_DIRECTORY = g_currentModDirectory,
 	areDuplicatesAllowed = false,
@@ -29,57 +29,58 @@ function LicensePlateStorage.enableLicensePlateDuplicates(_,enable)
 	LicensePlateStorage.debug("Duplicates are %s", tostring(LicensePlateStorage.areDuplicatesAllowed))
 end
 
---- Adds the license plate str to a global table.
-local function setLicensePlatesData(vehicle, licensePlateData, ...)
-	if vehicle:getHasLicensePlates() and vehicle.propertyState ~= Vehicle.PROPERTY_STATE_SHOP_CONFIG then
-		if licensePlateData and licensePlateData.characters then 
-			local chars = table.concat(licensePlateData.characters)
-			if chars then 
-				LicensePlateStorage.debug("found license plate data (%s)) for %s", chars, vehicle:getName())
-				if LicensePlateStorage.data[chars] == nil then 
-					LicensePlateStorage.data[chars] = {}
-				end
-				LicensePlateStorage.data[chars][vehicle] = vehicle				
-				local spec = vehicle.spec_licensePlates
+-- --- Adds the license plate str to a global table.
+-- local function setLicensePlatesData(vehicle, licensePlateData, ...)
+-- 	if vehicle:getHasLicensePlates() and vehicle.propertyState ~= Vehicle.PROPERTY_STATE_SHOP_CONFIG then
+-- 		if licensePlateData and licensePlateData.characters then 
+-- 			local chars = table.concat(licensePlateData.characters)
+-- 			if chars then 
+-- 				LicensePlateStorage.debug("found license plate data (%s) for %s(%s)", 
+-- 					chars,  vehicle:getName(), vehicle.rootVehicle:getName())
+-- 				if LicensePlateStorage.data[chars] == nil then 
+-- 					LicensePlateStorage.data[chars] = {}
+-- 				end
+-- 				LicensePlateStorage.data[chars][vehicle] = vehicle				
+-- 				local spec = vehicle.spec_licensePlates
 
-				if spec.lpsOldLicensePlateData and spec.lpsOldLicensePlateData.characters then 
-					local chars = table.concat(spec.lpsOldLicensePlateData.characters)
-					if chars then
-						LicensePlateStorage.data[chars][vehicle] = nil	
-						if next(LicensePlateStorage.data[chars]) == nil then 
-							LicensePlateStorage.data[chars] = nil
-						end
-					end
-				end
-				spec.lpsOldLicensePlateData = table.copy(licensePlateData, 3)
-			end
-		end
-	end
-end
+-- 				if spec.lpsOldLicensePlateData and spec.lpsOldLicensePlateData.characters then 
+-- 					local chars = table.concat(spec.lpsOldLicensePlateData.characters)
+-- 					if chars then
+-- 						LicensePlateStorage.data[chars][vehicle] = nil	
+-- 						if next(LicensePlateStorage.data[chars]) == nil then 
+-- 							LicensePlateStorage.data[chars] = nil
+-- 						end
+-- 					end
+-- 				end
+-- 				spec.lpsOldLicensePlateData = table.clone(licensePlateData, 3)
+-- 			end
+-- 		end
+-- 	end
+-- end
 
-LicensePlates.setLicensePlatesData = Utils.appendedFunction(LicensePlates.setLicensePlatesData, setLicensePlatesData)
+-- LicensePlates.setLicensePlatesData = Utils.appendedFunction(LicensePlates.setLicensePlatesData, setLicensePlatesData)
 
---- Removes the license plate str from the global table, as the vehicles was deleted.
-local function removeLicensePlate(vehicle, ...)
-	if vehicle.propertyState ~= Vehicle.PROPERTY_STATE_SHOP_CONFIG then
-		local spec = vehicle.spec_licensePlates
-		if spec and spec.licensePlateData and spec.licensePlateData.characters then
-			local chars = table.concat(spec.licensePlateData.characters)
-			LicensePlateStorage.debug("trying to remove %s", tostring(chars))
-			if chars and LicensePlateStorage.data[chars] then 
-				LicensePlateStorage.data[chars][vehicle] = nil
-				if next(LicensePlateStorage.data[chars]) == nil then 
-					LicensePlateStorage.data[chars] = nil
-				end
-				LicensePlateStorage.debug("removed license plate data for %s", vehicle:getName())
-			end
-		end
-	end	
-end
+-- --- Removes the license plate str from the global table, as the vehicles was deleted.
+-- local function removeLicensePlate(vehicle, ...)
+-- 	if vehicle.propertyState ~= Vehicle.PROPERTY_STATE_SHOP_CONFIG then
+-- 		local spec = vehicle.spec_licensePlates
+-- 		if spec and spec.licensePlateData and spec.licensePlateData.characters then
+-- 			local chars = table.concat(spec.licensePlateData.characters)
+-- 			LicensePlateStorage.debug("trying to remove %s", tostring(chars))
+-- 			if chars and LicensePlateStorage.data[chars] then 
+-- 				LicensePlateStorage.data[chars][vehicle] = nil
+-- 				if next(LicensePlateStorage.data[chars]) == nil then 
+-- 					LicensePlateStorage.data[chars] = nil
+-- 				end
+-- 				LicensePlateStorage.debug("removed license plate data for %s", vehicle:getName())
+-- 			end
+-- 		end
+-- 	end	
+-- end
 
-LicensePlates.onDelete = Utils.prependedFunction(LicensePlates.onDelete, removeLicensePlate)
+-- LicensePlates.onDelete = Utils.prependedFunction(LicensePlates.onDelete, removeLicensePlate)
 
---- Checks if the plate str is already given to another vehicle.
+-- --- Checks if the plate str is already given to another vehicle.
 function LicensePlateStorage.validatePlates(licensePlateData, curVehicle, storeItem)
 	LicensePlateStorage.debug("validatePlates")
 	local valid, vehicleFound = true, nil
@@ -116,46 +117,68 @@ function LicensePlateStorage.validatePlates(licensePlateData, curVehicle, storeI
 	return valid
 end
 
---- Only allow license plate change, if the new plate is not given to another vehicle.
-local function onChangeLicensePlate(self, superFunc, licensePlateData, ...)
-	if not g_licensePlateManager:getAreLicensePlatesAvailable() then 
-		return superFunc(self, licensePlateData, ...)
-	end
-	if licensePlateData == nil or next(licensePlateData) == nil then 
-		return superFunc(self, licensePlateData, ...)
-	end
+-- --- Only allow license plate change, if the new plate is not given to another vehicle.
+-- local function onChangeLicensePlate(self, superFunc, licensePlateData, ...)
+-- 	if not g_licensePlateManager:getAreLicensePlatesAvailable() then 
+-- 		return superFunc(self, licensePlateData, ...)
+-- 	end
+-- 	if licensePlateData == nil or next(licensePlateData) == nil then 
+-- 		return superFunc(self, licensePlateData, ...)
+-- 	end
 	
-	local valid = LicensePlateStorage.validatePlates(licensePlateData, self.vehicle, self.storeItem)
-	if not valid then 
-		return
-	end
-	return superFunc(self, licensePlateData, ...)
-end
-ShopConfigScreen.onChangeLicensePlate = Utils.overwrittenFunction(ShopConfigScreen.onChangeLicensePlate, onChangeLicensePlate)
+-- 	local valid = LicensePlateStorage.validatePlates(licensePlateData, self.vehicle, self.storeItem)
+-- 	if not valid then 
+-- 		return
+-- 	end
+-- 	return superFunc(self, licensePlateData, ...)
+-- end
+-- ShopConfigScreen.onChangeLicensePlate = Utils.overwrittenFunction(ShopConfigScreen.onChangeLicensePlate, onChangeLicensePlate)
 
 --- Adds a menu to view all assigned license plates.
 local function onOpen(self)
 	self.target.sortedPlates = {}
 	self.target.sortedVehicles = {}
-
-	--- Sorts the license plate strings.
-	self.target.data = {}
-	for str, vehicleTable in pairs(LicensePlateStorage.data) do 
-		for _, v in pairs(vehicleTable) do
-			str = LicensePlates.getSpecValuePlateText(nil, v) or str
-			table.insert(self.target.sortedPlates, str)
-			if self.target.data[str] == nil then 
-				self.target.data[str] = {}
+	for _, v in pairs(g_currentMission.vehicleSystem.vehicles) do
+		local str = ""
+		local spec = v.spec_licensePlates
+		if spec ~= nil then
+			if v:getHasLicensePlates() and v.propertyState ~= Vehicle.PROPERTY_STATE_SHOP_CONFIG then
+				local licensePlateData = spec.licensePlateData
+				if licensePlateData and licensePlateData.characters then 
+					str = table.concat(licensePlateData.characters)
+					if str then 
+						LicensePlateStorage.debug("found license plate data (%s) for %s(%s)", 
+							str,  v:getName(), v.rootVehicle:getName())	
+						
+					end
+				end
 			end
-			table.insert(self.target.data[str], v)
+			if str ~= "" then 
+				table.insert(self.target.sortedPlates, 
+					LicensePlates.getSpecValuePlateText(nil, v) or str)
+				table.insert(self.target.sortedVehicles, v)
+			end
 		end
 	end
-	table.sort(self.target.sortedPlates)
-	for _, str in ipairs(self.target.sortedPlates) do 
-		for _,v in ipairs(self.target.data[str]) do 
-			table.insert(self.target.sortedVehicles, v)
-		end
-	end
+
+	-- --- Sorts the license plate strings.
+	-- self.target.data = {}
+	-- for str, vehicleTable in pairs(LicensePlateStorage.data) do 
+	-- 	for _, v in pairs(vehicleTable) do
+	-- 		str = LicensePlates.getSpecValuePlateText(nil, v) or str
+	-- 		table.insert(self.target.sortedPlates, str)
+	-- 		if self.target.data[str] == nil then 
+	-- 			self.target.data[str] = {}
+	-- 		end
+	-- 		table.insert(self.target.data[str], v)
+	-- 	end
+	-- end
+	-- table.sort(self.target.sortedPlates)
+	-- for _, str in ipairs(self.target.sortedPlates) do 
+	-- 	for _,v in ipairs(self.target.data[str]) do 
+	-- 		table.insert(self.target.sortedVehicles, v)
+	-- 	end
+	-- end
 	
 	if not self.tpsInitialized then 
 		--- Functions needed for the smooth list.
@@ -166,13 +189,19 @@ local function onOpen(self)
 		self.target.populateCellForItemInSection = function (self, list, section, index, cell)
 			local plate = self.sortedPlates[index]
 			local vehicle = self.sortedVehicles[index]
-			cell:getAttribute("vehicle"):setText(vehicle:getName())
-			cell:getAttribute("licensePlate"):setText(plate)
-			if #self.data[plate] > 1 and not LicensePlateStorage.areDuplicatesAllowed then 
-				cell:setDisabled(true)
-			else 
-				cell:setDisabled(false)
+			if vehicle.rootVehicle:getName() ~= vehicle:getName() then
+				cell:getAttribute("vehicle"):setText(vehicle.rootVehicle:getName())
+				cell:getAttribute("implement"):setText(vehicle:getName())
+			else
+				cell:getAttribute("vehicle"):setText(vehicle:getName())
+				cell:getAttribute("implement"):setText("")
 			end
+			cell:getAttribute("licensePlate"):setText(plate)
+			-- if #self.data[plate] > 1 and not LicensePlateStorage.areDuplicatesAllowed then 
+			-- 	cell:setDisabled(true)
+			-- else 
+			-- 	cell:setDisabled(false)
+			-- end
 		end
 		
 		self.target.onListSelectionChanged = function(self, list, section, index)
@@ -181,6 +210,10 @@ local function onOpen(self)
 		self.target.getNumberOfSections = function(self)
 			return 1
 		end
+		
+		-- self.target.getTitleForSectionHeader = function(self)
+		-- 	return ""
+		-- end
 
 		self.target.onClickAllowDuplicates = function(self, btn)
 			LicensePlateStorage.areDuplicatesAllowed = not LicensePlateStorage.areDuplicatesAllowed
@@ -199,29 +232,28 @@ local function onOpen(self)
 		--- Adds the additional menu.
 		LicensePlateStorage.debug("self.tpsInitialized")
 		g_gui:loadProfiles( Utils.getFilename("gui/guiProfiles.xml", LicensePlateStorage.BASE_DIRECTORY) )
-		self.target:registerControls({"licensePlateLayout", "tableHeaderBox", "licensePlateList", "allDuplicatesBtn"})
 
 		local xmlFile = loadXMLFile("Temp", Utils.getFilename("gui/AssignedLicensePlates.xml", LicensePlateStorage.BASE_DIRECTORY))
-		g_gui:loadGuiRec(xmlFile, "AssignedLicensePlatesLayout", self, self.target)
+		g_gui:loadGuiRec(xmlFile, "AssignedLicensePlatesLayout", self, self.target)		
 		delete(xmlFile)
 		--- Setup for the list and the additional gui elements.
 		self.target:exposeControlsAsFields()
 		self.tpsInitialized = true
 		self.target.licensePlateList:setDataSource(self.target)
 	  	self.target.licensePlateList:setDelegate(self.target)
-		self.target.licensePlateLayout:onGuiSetupFinished()
+		self.target:onGuiSetupFinished()
 		self:updateAbsolutePosition()
 
-		self.target.allDuplicatesBtn:unlinkElement()
-		FocusManager:removeElement(self.target.allDuplicatesBtn)
-		self.target.okButton.parent:addElement(self.target.allDuplicatesBtn)
-		self.target.okButton.parent:invalidateLayout()
-		self.target.allDuplicatesBtn:setText(LicensePlateStorage.getText())
+	-- 	self.target.allDuplicatesBtn:unlinkElement()
+	-- 	FocusManager:removeElement(self.target.allDuplicatesBtn)
+	-- 	self.target.okButton.parent:addElement(self.target.allDuplicatesBtn)
+	-- 	self.target.okButton.parent:invalidateLayout()
+	-- 	self.target.allDuplicatesBtn:setText(LicensePlateStorage.getText())
 	end
 	self.target.licensePlateList:reloadData()
 end
 
-local function onLoadMapFinished(menu, ...)
+local function onLoadMapFinished(configScreen, ...)
 	--- Only allow buying, if the license plate is not given to another vehicle.
 	local function onClick(self, superFunc, ...)
 		if not g_licensePlateManager:getAreLicensePlatesAvailable()  then 
@@ -236,12 +268,10 @@ local function onLoadMapFinished(menu, ...)
 		end
 		return superFunc(self, ...)
 	end
-	menu.shopConfigScreen.buyButton.onClickCallback = Utils.overwrittenFunction(
-										menu.shopConfigScreen.buyButton.onClickCallback,
-										onClick)
-	menu.shopConfigScreen.leaseButton.onClickCallback = Utils.overwrittenFunction(
-											menu.shopConfigScreen.leaseButton.onClickCallback,
-											onClick)
+	configScreen.buyButton.onClickCallback = Utils.overwrittenFunction(
+		configScreen.buyButton.onClickCallback, onClick)
+	configScreen.leaseButton.onClickCallback = Utils.overwrittenFunction(
+		configScreen.leaseButton.onClickCallback, onClick)
 
 	--- Adds the additional license plate menu.
 	g_gui.guis.LicensePlateDialog.onOpen = Utils.prependedFunction(g_gui.guis.LicensePlateDialog.onOpen, onOpen)
@@ -265,4 +295,4 @@ local function onLoadMapFinished(menu, ...)
 	end
 
 end
-ShopMenu.onLoadMapFinished = Utils.appendedFunction(ShopMenu.onLoadMapFinished, onLoadMapFinished)
+ShopConfigScreen.onFinishedLoading = Utils.appendedFunction(ShopConfigScreen.onFinishedLoading, onLoadMapFinished)
